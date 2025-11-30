01238b10e2399ee0431e460c0ec7ad9a45456246
Masi Woermann <masi.woermann@gmail.com>
2025-11-30 16:19:53 +0000

    Removing ignore directories

--- dist/TetrisEvent.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class TetrisEvent extends Event {
-    static readonly GAME_STARTED = "gameStarted";
-    static readonly GAME_OVER = "gameOver";
-    static readonly LEVEL_UP = "levelUp";
-    static readonly SCORE_UPDATED = "scoreUpdated";
-    constructor(type: string);
-}
-//# sourceMappingURL=TetrisEvent.d.ts.map
